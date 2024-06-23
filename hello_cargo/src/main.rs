6b6164c3a91d6a44d2c39ed3f81b26207ecc6a49
Ian Kosen <ikosenn@gmail.com>
2024-06-22 17:04:21 -0700

    modules

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -5,4 +5,11 @@ fn main() {
     let a = [String::new()];
     let b = a;
     println!("{:?}", b);
+
+    // test
+    let s1 = String::from("Hello, ");
+    let mut s2 = String::from("world!");
+    let s3 = s1 + &s2;
+    s2 = "John";
+    println!("string is {s3}");
 }
